7a7c0897eb7548b6e2767f1225b324e5142e2448
Rayka Agustasya <raykaagustasya16@gmail.com>
2021-01-13 12:03:48 +0700

    Add project files.

---
+++ WCFService_2Way_017/ServiceCallback.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFService_2Way_017
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class ServiceCallback : IClientCallback
+    {
+        Dictionary<IClientCallback, string> userList = new Dictionary<IClientCallback, string>();
+        public void gabung(string username)
+        {
+            IClientCallback koneksiGabung = OperationContext.Current.GetCallbackChannel<IClientCallback>();
+            userList[koneksiGabung] = username;
+        }
+
+        public void kirimPesan(string pesan)
+        {
+            IClientCallback koneksiPesan = OperationContext.Current.GetCallbackChannel<IClientCallback>();
+            string user;
+            if (userList.TryGetValue(koneksiPesan, out user))
+            {
+                return;
+            }
+            foreach (IClientCallback other in userList.Keys)
+            {
+                other.kirimPesan(user, pesan);
+            }
+        }
+
+        public void kirimPesan(string pesan, string pesan1)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
